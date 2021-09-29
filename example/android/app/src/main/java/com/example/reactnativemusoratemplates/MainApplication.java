84119734386b60d86fadc6751e77218aaa01a3ea
ARTSOFT\csillaj <jenei_csilla@yahoo.com>
2021-09-29 16:20:01 +0300

    * android installation for react-native-android-keyboard-adjust

--- example/android/app/src/main/java/com/example/reactnativemusoratemplates/MainApplication.java
+++ example/android/app/src/main/java/com/example/reactnativemusoratemplates/MainApplication.java
@@ -32,7 +32,7 @@ import com.reactnative.googlecast.GoogleCastPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.reactnativecommunity.netinfo.NetInfoPackage;
 import com.eko.RNBackgroundDownloaderPackage;
-
+import net.zubricky.AndroidKeyboardAdjust.AndroidKeyboardAdjustPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -68,6 +68,7 @@ public class MainApplication extends Application implements ReactApplication {
           packages.add(new ReactVideoPackage());
           packages.add(new NetInfoPackage());
           packages.add(new RNBackgroundDownloaderPackage());
+          packages.add(new AndroidKeyboardAdjustPackage());
           return packages;
         }
 
